0a7ec9ebb169c766652893eb6c534390af1426cd
Thomas Schlott <thomas.schlott@outlook.de>
2024-03-13 22:49:15 +0100

    Removed unused usings

--- TicketModel/Ticket.cs
+++ TicketModel/Ticket.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace TinyTicketSystem
 {
