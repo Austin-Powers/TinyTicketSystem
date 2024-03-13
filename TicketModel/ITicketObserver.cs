041de762b7db3b49ab672589260947f6b17c3129
Thomas Schlott <thomas.schlott@outlook.de>
2024-03-14 00:38:17 +0100

    Added ITicketObserver interface

---
+++ TicketModel/ITicketObserver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TinyTicketSystem;
+
+namespace TicketModel
+{
+    /// <summary>
+    /// Interface for classes observing changes on a ticket.
+    /// </summary>
+    internal interface ITicketObserver
+    {
+        /// <summary>
+        /// Method called if a ticket is updated.
+        /// </summary>
+        /// <param name="ticket">The ticket that has been updated.</param>
+        void OnTicketChanged(Ticket ticket);
+    }
+}
